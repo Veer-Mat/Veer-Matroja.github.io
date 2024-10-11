e9156241b3c56655ad858a48a033905938614d46
Veer-Mat <vmatroja@torontomu.ca>
2024-10-11 00:12:31 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function nameofUser() {
+    let userName = prompt("please enter your name:");
+
+    if (userName) {
+      document.getElementById("output").innerHTML = "Hello, "+ userName;
+    }
+  }
+
+//Function to adjust Image
+  function adjustImage() {
+    let image = document.getElementById("imageTemp");
+    image.classList.remove("image")
+    image.classList.add("tweakedImage") //calls on ImageTweak in CSS
+}
