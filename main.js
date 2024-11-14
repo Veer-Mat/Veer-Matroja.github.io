21ead600375c6804ae580456f7423f094ae8ba25
Veer-Mat <vmatroja@torontomu.ca>
2024-11-14 03:03:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -13,3 +13,79 @@ function nameofUser() {
     image.classList.add("tweakedImage") //calls on ImageTweak in CSS
 }
 
+
+
+
+
+function playandpause()
+{
+  const myVideo = document.getElementById("video1")
+  if (myVideo.paused)
+    {
+      myVideo.play();
+  } else
+  {
+    myVideo.pause();
+  }
+}
+
+function Big()
+{
+  const video = document.getElementById("video1")
+  video.width = 700;
+  video.height = 600;
+}
+
+function Small()
+{
+  const video = document.getElementById("video1")
+  video.width = 320;
+  video.height = 100;
+}
+
+function Normal()
+{
+  const video = document.getElementById("video1")
+  video.width = 560;
+  video.height = 350;
+}
+
+function showAltText(elementId) {
+  const element = document.getElementById(elementId);
+  let altText;
+
+  if (element.tagName == 'IMG') {
+    altText = element.alt;
+
+    alert(` ${altText}`);
+  }
+}
+
+function IncreaseText() {
+  const textElements = document.querySelectorAll('#effect');
+  textElements.forEach(element => {
+      const currentSize = window.getComputedStyle(element).fontSize;
+      const newSize = parseFloat(currentSize) + 5;
+      element.style.fontSize = newSize + 'px';
+  });
+}
+
+
+function decreaseText() {
+  const textElements = document.querySelectorAll('#effect');
+  textElements.forEach(element => {
+      const currentSize = window.getComputedStyle(element).fontSize;
+      const newSize = parseFloat(currentSize) - 5;
+      element.style.fontSize = newSize + 'px';
+  });
+}
+
+function colorChangeText() {
+  const elements = document.querySelectorAll('#effect');
+    elements.forEach(function(element) { element.style.color = 'Red';});
+}
+
+function textResetColor() {
+  const elements = document.querySelectorAll('#effect');
+    elements.forEach(function(element) { element.style.color = '';});
+}
